b36245e545ced26aa6917832b49e5b025e5eda2f
Charles Howard <cdhoward@add123.com>
2016-03-24 08:04:13 -0400

    worms texture now working, mod renamed to fishingmod and Worm renamed to worms

--- src/main/java/com/frodare/fishing/FishingMod.java
+++ src/main/java/com/frodare/fishing/FishingMod.java
@@ -1,9 +1,11 @@
 package com.frodare.fishing;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.RenderItem;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
@@ -12,7 +14,10 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.fml.relauncher.Side;
 
+import com.frodare.baitmod.SugarBlock;
+import com.frodare.baitmod.SugarBlockTileEntity;
 import com.frodare.fishing.events.EventHooks;
 import com.frodare.fishing.items.Worms;
 
@@ -30,13 +35,28 @@ public class FishingMod {
 	
 	@EventHandler
 	public void preInit(FMLPreInitializationEvent event) {
-		worms = new Worms();
 		
-		GameRegistry.registerItem(worms, "worms");
-		ModelResourceLocation wormsModel = new ModelResourceLocation("fishingmod:worms", "inventory");
 		
-		Minecraft.getMinecraft().getRenderItem().getItemModelMesher().register(worms, 0, wormsModel);
+		
 	}
+	
+	@EventHandler
+	public void init(FMLInitializationEvent event) {
+		worms = new Worms();
+		GameRegistry.registerItem(worms, Worms.NAME);
+		
+		//ModelResourceLocation wormsModel = new ModelResourceLocation("fishingmod:worms", "inventory");
+		//Minecraft.getMinecraft().getRenderItem().getItemModelMesher().register(worms, 0, wormsModel);
+		
+		if(event.getSide() == Side.CLIENT){
+			RenderItem renderItem = Minecraft.getMinecraft().getRenderItem();
+			renderItem.getItemModelMesher().register(worms, 0, new ModelResourceLocation(FishingMod.MODID + ":" + Worms.NAME, "inventory"));
+			
+			
+		}
+	}
+	
+	
 	
 	@EventHandler
 	public void load(FMLInitializationEvent event) {
--- src/main/java/com/frodare/fishing/items/Worms.java
+++ src/main/java/com/frodare/fishing/items/Worms.java
@@ -4,9 +4,11 @@ import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 
 public class Worms extends Item {
+	
+	public static String NAME = "worms";
 
 	public Worms() {
-		this(64, CreativeTabs.tabMisc, "worms");
+		this(64, CreativeTabs.tabMisc, NAME);
 	}
 	
 	public Worms(int maxStackSize, CreativeTabs tab, String name) {

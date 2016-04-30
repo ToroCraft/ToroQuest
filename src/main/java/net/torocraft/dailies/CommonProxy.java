aa495da392a54ad8cec7f0f3ae1e7dcff12b97d1
Charles Howard <cdhoward@add123.com>
2016-04-30 07:48:28 -0400

    adding dailies mod placeholder

---
+++ src/main/java/net/torocraft/dailies/CommonProxy.java
@@ -0,0 +1,19 @@
+package net.torocraft.dailies;
+
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+public class CommonProxy {
+    public void preInit(FMLPreInitializationEvent e) {
+
+    }
+
+    public void init(FMLInitializationEvent e) {
+
+    }
+
+    public void postInit(FMLPostInitializationEvent e) {
+
+    }
+}
---
+++ src/main/java/net/torocraft/dailies/DailiesMod.java
@@ -0,0 +1,39 @@
+package net.torocraft.dailies;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod (modid = DailiesMod.MODID, name = DailiesMod.MODNAME, version = DailiesMod.VERSION)
+public class DailiesMod {
+
+	
+	public static final String MODID = "dailies";
+	public static final String VERSION = "1.0";
+	public static final String MODNAME = "DailiesMod";
+	
+	@SidedProxy(clientSide="net.torocraft.dailies.ClientProxy", serverSide="net.torocraft.dailies.ServerProxy")
+	public static CommonProxy proxy;
+		
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent e) {
+	    proxy.preInit(e);
+	}
+
+	@EventHandler
+	public void init(FMLInitializationEvent e) {
+	    proxy.init(e);
+	}
+
+	@EventHandler
+	public void postInit(FMLPostInitializationEvent e) {
+	    proxy.postInit(e);
+	}
+	
+	
+	
+	
+}

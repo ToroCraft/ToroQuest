99b07ba3b6ae5c1863f580ce745324d9e9b4abf4
Zane Riley <zriley@add123.com>
2016-05-11 10:30:42 -0400

    added king armor recipes

--- src/main/java/net/torocraft/torobasemod/crafting/ToroBaseModRecipes.java
+++ src/main/java/net/torocraft/torobasemod/crafting/ToroBaseModRecipes.java
@@ -4,12 +4,22 @@ import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.torocraft.torobasemod.item.armor.ItemHeavyDiamodArmor;
+import net.torocraft.torobasemod.item.armor.ItemKingArmor;
 
 public class ToroBaseModRecipes {
 	
 	public static final void init() {
 		heavyDiamondArmor();
-		
+		kingArmor();
+	}
+	
+	private static void kingArmor() {
+		ItemStack goldIngot = new ItemStack(Items.gold_ingot);
+		ItemStack diamond = new ItemStack(Items.diamond);
+		GameRegistry.addRecipe(new ItemStack(ItemKingArmor.helmetItem), "ggg", "ddd", "d d", 'g', goldIngot, 'd', diamond);
+		GameRegistry.addRecipe(new ItemStack(ItemKingArmor.chestplateItem), "dgd", "ddd", "ddd", 'g', goldIngot, 'd', diamond);
+		GameRegistry.addRecipe(new ItemStack(ItemKingArmor.leggingsItem), "ddd", "dgd", "dgd", 'g', goldIngot, 'd', diamond);
+		GameRegistry.addRecipe(new ItemStack(ItemKingArmor.bootsItem), "g g", "d d", "d d", 'g', goldIngot, 'd', diamond);
 	}
 
 	private static void heavyDiamondArmor() {

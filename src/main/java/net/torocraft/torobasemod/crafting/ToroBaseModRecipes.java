75737198bea88a71ea0cedd025e4d1f830c6ee3d
Charles Howard <cdhoward@add123.com>
2016-05-13 20:40:33 -0400

    more samurai armor work

--- src/main/java/net/torocraft/torobasemod/crafting/ToroBaseModRecipes.java
+++ src/main/java/net/torocraft/torobasemod/crafting/ToroBaseModRecipes.java
@@ -3,10 +3,9 @@ package net.torocraft.torobasemod.crafting;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.registry.GameRegistry;
-import net.torocraft.torobasemod.ToroBaseMod;
-import net.torocraft.torobasemod.item.ToroBaseModItems;
 import net.torocraft.torobasemod.item.armor.ItemHeavyDiamondArmor;
 import net.torocraft.torobasemod.item.armor.ItemKingArmor;
+import net.torocraft.torobasemod.item.armor.ItemSamuraiArmor;
 import net.torocraft.torobasemod.item.projectile.ItemTorchArrow;
 
 public class ToroBaseModRecipes {
@@ -15,6 +14,7 @@ public class ToroBaseModRecipes {
 		heavyDiamondArmor();
 		kingArmor();
 		torchArrow();
+		samuraiArmor();
 	}
 	
 	private static void torchArrow() {
@@ -34,6 +34,15 @@ public class ToroBaseModRecipes {
 		GameRegistry.addRecipe(new ItemStack(ItemKingArmor.chestplateItem), "dgd", "ddd", "ddd", 'g', goldIngot, 'd', diamond);
 		GameRegistry.addRecipe(new ItemStack(ItemKingArmor.leggingsItem), "ddd", "dgd", "dgd", 'g', goldIngot, 'd', diamond);
 		GameRegistry.addRecipe(new ItemStack(ItemKingArmor.bootsItem), "g g", "d d", "d d", 'g', goldIngot, 'd', diamond);
+	}
+
+	private static void samuraiArmor() {
+		ItemStack emerald = new ItemStack(Items.emerald);
+		ItemStack leather = new ItemStack(Items.leather);
+		GameRegistry.addRecipe(new ItemStack(ItemSamuraiArmor.helmetItem), "ggg", "ddd", "d d", 'g', emerald, 'd', leather);
+		GameRegistry.addRecipe(new ItemStack(ItemSamuraiArmor.chestplateItem), "dgd", "ddd", "ddd", 'g', emerald, 'd', leather);
+		GameRegistry.addRecipe(new ItemStack(ItemSamuraiArmor.leggingsItem), "ddd", "dgd", "dgd", 'g', emerald, 'd', leather);
+		GameRegistry.addRecipe(new ItemStack(ItemSamuraiArmor.bootsItem), "dgd", "dgd", "   ", 'g', emerald, 'd', leather);
 	}
 
 	private static void heavyDiamondArmor() {

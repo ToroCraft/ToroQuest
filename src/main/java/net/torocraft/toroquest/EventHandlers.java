b2ddf8497b496df3806fd99b818984533943f722
Zane Riley <zeriley@gmail.com>
2016-09-11 18:26:10 -0400

    toro mob will spawn with cows sometimes; needs more work

--- src/main/java/net/torocraft/toroquest/EventHandlers.java
+++ src/main/java/net/torocraft/toroquest/EventHandlers.java
@@ -2,13 +2,35 @@ package net.torocraft.toroquest;
 
 import java.util.List;
 
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.passive.EntityCow;
+import net.minecraft.world.World;
+import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.torocraft.toroquest.entities.EntityToro;
 
 public class EventHandlers {
+
+	@SubscribeEvent
+	public void spawnToroWhenCowPackSpawns(EntityJoinWorldEvent event) {
+		Entity entity = event.getEntity();
+		if (entity == null || !(entity instanceof EntityCow)) {
+			return;
+		}
+
+		World world = event.getWorld();
+
+		if (world.rand.nextInt(8) != 0) {
+			return;
+		}
+
+		Entity toro = new EntityToro(world);
+
+		toro.setPosition(entity.posX + world.rand.nextInt(10), entity.posY, entity.posZ + world.rand.nextInt(10));
+		world.spawnEntityInWorld(toro);
+	}
 
 	@SubscribeEvent
 	public void toroDontLikeYouHurtingCows(LivingHurtEvent event) {
@@ -20,7 +42,8 @@ public class EventHandlers {
 			return;
 		}
 
-		List<EntityToro> nearbyToros = victum.getEntityWorld().getEntitiesWithinAABB(EntityToro.class, victum.getEntityBoundingBox().expand(40.0D, 10.0D, 40.0D));
+		List<EntityToro> nearbyToros = victum.getEntityWorld().getEntitiesWithinAABB(EntityToro.class,
+				victum.getEntityBoundingBox().expand(40.0D, 10.0D, 40.0D));
 		for (EntityToro toro : nearbyToros) {
 			toro.setAttackTarget(attacker);
 		}

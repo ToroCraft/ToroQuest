8da59cb74acad20db13b853d2fb2a07f5663e69a
Zane Riley <zeriley@gmail.com>
2016-09-11 21:43:32 -0400

    working on keeping the Toro from getting stuck in the ground

--- src/main/java/net/torocraft/toroquest/EventHandlers.java
+++ src/main/java/net/torocraft/toroquest/EventHandlers.java
@@ -2,9 +2,13 @@ package net.torocraft.toroquest;
 
 import java.util.List;
 
+import net.minecraft.block.BlockBush;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.passive.EntityCow;
+import net.minecraft.init.Blocks;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
@@ -26,24 +30,30 @@ public class EventHandlers {
 			return;
 		}
 
+		BlockPos spawnPoint = findSurface(world, entity.getPosition());
+
+		if (spawnPoint == null) {
+			return;
+		}
+
 		Entity toro = new EntityToro(world);
 
-		toro.setPosition(entity.posX + world.rand.nextInt(10), entity.posY, entity.posZ + world.rand.nextInt(10));
+		toro.setPosition(spawnPoint.getX(), spawnPoint.getY() + 1, spawnPoint.getZ());
 		world.spawnEntityInWorld(toro);
 	}
 
 	@SubscribeEvent
 	public void toroDontLikeYouHurtingCows(LivingHurtEvent event) {
 		System.out.println("You hurt a cow?");
-		EntityLivingBase victum = event.getEntityLiving();
+		EntityLivingBase victim = event.getEntityLiving();
 		EntityLivingBase attacker = getAttacker(event);
 
-		if (victum == null || attacker == null || !(victum instanceof EntityCow)) {
+		if (victim == null || attacker == null || !(victim instanceof EntityCow)) {
 			return;
 		}
 
-		List<EntityToro> nearbyToros = victum.getEntityWorld().getEntitiesWithinAABB(EntityToro.class,
-				victum.getEntityBoundingBox().expand(40.0D, 10.0D, 40.0D));
+		List<EntityToro> nearbyToros = victim.getEntityWorld().getEntitiesWithinAABB(EntityToro.class,
+				victim.getEntityBoundingBox().expand(40.0D, 10.0D, 40.0D));
 		for (EntityToro toro : nearbyToros) {
 			toro.setAttackTarget(attacker);
 		}
@@ -55,6 +65,50 @@ public class EventHandlers {
 		} catch (Exception e) {
 			return null;
 		}
+	}
+
+	private BlockPos findSurface(World world, BlockPos start) {
+
+		int minY = world.getActualHeight();
+
+		BlockPos pos;
+
+		IBlockState blockState;
+
+		for (int y = world.getActualHeight(); y > 0; y--) {
+
+			pos = new BlockPos(start.getX(), y, start.getZ());
+			blockState = world.getBlockState(pos);
+
+			if (isLiquid(blockState)) {
+				return null;
+			}
+
+			if (isGroundBlock(blockState)) {
+				if (y < minY) {
+					minY = y;
+				}
+
+				break;
+			}
+		}
+
+		return new BlockPos(start.getX(), minY, start.getZ());
+	}
+
+	private boolean isLiquid(IBlockState blockState) {
+		return blockState.getBlock() == Blocks.WATER || blockState.getBlock() == Blocks.LAVA;
+	}
+
+	private boolean isGroundBlock(IBlockState blockState) {
+
+		if (blockState.getBlock() == Blocks.LEAVES || blockState.getBlock() == Blocks.LEAVES2
+				|| blockState.getBlock() == Blocks.LOG || blockState.getBlock() instanceof BlockBush) {
+			return false;
+		}
+
+		return blockState.isOpaqueCube();
+
 	}
 
 }

3a51b77f4a0d1bf8c12adcb1329fe001e0cd036f
Charles Howard <cdhoward@add123.com>
2016-09-21 22:49:58 -0400

    seeing if LayerCape would be used for the guards

--- src/main/java/net/torocraft/toroquest/entities/model/ModelGuard.java
+++ src/main/java/net/torocraft/toroquest/entities/model/ModelGuard.java
@@ -1,48 +1,41 @@
 package net.torocraft.toroquest.entities.model;
 
 import net.minecraft.client.model.ModelBiped;
+import net.minecraft.client.model.ModelRenderer;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.monster.EntityZombie;
-import net.minecraft.util.math.MathHelper;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
 @SideOnly(Side.CLIENT)
 public class ModelGuard extends ModelBiped {
+
+	private final ModelRenderer bipedCape;
+
 	public ModelGuard() {
 		this(0.0F);
 	}
 
 	public ModelGuard(float modelSize) {
 		super(modelSize, 0.0F, 64, 64);
+		this.bipedCape = new ModelRenderer(this, 0, 0);
+		this.bipedCape.setTextureSize(64, 32);
+		this.bipedCape.addBox(-5.0F, 0.0F, -1.0F, 10, 16, 1, modelSize);
+
 	}
 
 	public void setRotationAngles(float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch, float scaleFactor, Entity entityIn) {
 		super.setRotationAngles(limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, scaleFactor, entityIn);
-		
-		// zombieArmLogic(ageInTicks, entityIn);
+
+		if (entityIn.isSneaking()) {
+			this.bipedCape.rotationPointY = 2.0F;
+		} else {
+			this.bipedCape.rotationPointY = 0.0F;
+		}
 
 	}
 
-	protected void zombieArmLogic(float ageInTicks, Entity entityIn) {
-		boolean flag = entityIn instanceof EntityZombie && ((EntityZombie) entityIn).isArmsRaised();
-		
-
-		float f = MathHelper.sin(swingProgress * (float) Math.PI);
-		float f1 = MathHelper.sin((1.0F - (1.0F - swingProgress) * (1.0F - swingProgress)) * (float) Math.PI);
-		
-		bipedRightArm.rotateAngleZ = 0.0F;
-		bipedLeftArm.rotateAngleZ = 0.0F;
-		bipedRightArm.rotateAngleY = -(0.1F - f * 0.6F);
-		bipedLeftArm.rotateAngleY = 0.1F - f * 0.6F;
-		float f2 = -(float) Math.PI / (flag ? 1.5F : 2.25F);
-		bipedRightArm.rotateAngleX = f2;
-		bipedLeftArm.rotateAngleX = f2;
-		bipedRightArm.rotateAngleX += f * 1.2F - f1 * 0.4F;
-		bipedLeftArm.rotateAngleX += f * 1.2F - f1 * 0.4F;
-		bipedRightArm.rotateAngleZ += MathHelper.cos(ageInTicks * 0.09F) * 0.05F + 0.05F;
-		bipedLeftArm.rotateAngleZ -= MathHelper.cos(ageInTicks * 0.09F) * 0.05F + 0.05F;
-		bipedRightArm.rotateAngleX += MathHelper.sin(ageInTicks * 0.067F) * 0.05F;
-		bipedLeftArm.rotateAngleX -= MathHelper.sin(ageInTicks * 0.067F) * 0.05F;
+	public void renderCape(float scale) {
+		this.bipedCape.render(scale);
 	}
+
 }

2359b8a5fbbd65d1a28eda7c8c505b5e10f424ca
Charles Howard <cdhoward@add123.com>
2016-09-27 19:59:43 -0400

    adding all the DLD handlers

--- src/main/java/net/torocraft/toroquest/civilization/CivilizationGeneratorHandlers.java
+++ src/main/java/net/torocraft/toroquest/civilization/CivilizationGeneratorHandlers.java
@@ -2,7 +2,6 @@ package net.torocraft.toroquest.civilization;
 
 import jaredbgreat.dldungeons.api.DLDEvent;
 import net.minecraft.init.Blocks;
-import net.minecraft.util.math.BlockPos;
 import net.minecraftforge.event.terraingen.PopulateChunkEvent;
 import net.minecraftforge.fml.common.Optional.Method;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -19,22 +18,65 @@ public class CivilizationGeneratorHandlers {
 
 	@Method(modid = "dldungeonsjdg")
 	@SubscribeEvent
-	public void spawnDunguen(DLDEvent.Spawn event) {
-		System.out.println("************ DLDEvent.Spawn");
+	public void spawnDunguen(DLDEvent.placeDungeon event) {
+
+	}
+
+	@Method(modid = "dldungeonsjdg")
+	@SubscribeEvent
+	public void spawnDunguen(DLDEvent.BeforeBuild event) {
+		// printFloorPlan(event);
+	}
+
+	@Method(modid = "dldungeonsjdg")
+	@SubscribeEvent
+	public void placeDungeonBlock(DLDEvent.AddTileEntitiesToRoom event) {
+
+	}
+
+	@Method(modid = "dldungeonsjdg")
+	@SubscribeEvent
+	public void placeDungeonBlock(DLDEvent.AddChestBlocksToRoom event) {
+
+	}
+
+	@Method(modid = "dldungeonsjdg")
+	@SubscribeEvent
+	public void placeDungeonBlock(DLDEvent.AddEntrance event) {
+
 	}
 
 	@Method(modid = "dldungeonsjdg")
 	@SubscribeEvent
 	public void placeDungeonBlock(DLDEvent.PlaceBlock event) {
+		// replaceSpawnersWithDiamond(event);
+	}
 
-		System.out.println("event caught to place " + event.getBlock().getUnlocalizedName());
-
+	protected void replaceSpawnersWithDiamond(DLDEvent.PlaceBlock event) {
 		if (event.getBlock().getUnlocalizedName().equals("tile.mobSpawner")) {
-			System.out.println("*************** found spawner");
-			event.getWorld().setBlockState(new BlockPos(event.getX(), event.getY(), event.getZ()), Blocks.DIAMOND_BLOCK.getDefaultState());
+			event.getWorld().setBlockState(event.getPos(), Blocks.DIAMOND_BLOCK.getDefaultState());
 			event.setCanceled(true);
 		}
+	}
 
+	protected void printFloorPlan(DLDEvent.BeforeBuild event) {
+		int[][] room = event.getMapMatrix().room;
+		StringBuilder s = new StringBuilder();
+		s.append("\n");
+		for (int i = 0; i < room.length; i++) {
+			for (int j = 0; j < room.length; j++) {
+				s.append(pad(room[i][j]));
+			}
+			s.append("\n");
+		}
+		System.out.println(s.toString());
+	}
+
+	private Object pad(int i) {
+		if (i < 10) {
+			return "0" + i;
+		}
+		return i + "";
 	}
 
 }

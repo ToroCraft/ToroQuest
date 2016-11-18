ba0f9b7cfc3ad2bfc0ed0c5775b3f1d20dd22b2e
Charles Howard <cdhoward@add123.com>
2016-11-17 19:33:40 -0500

    version step to 3

--- src/main/java/net/torocraft/toroquest/ToroQuest.java
+++ src/main/java/net/torocraft/toroquest/ToroQuest.java
@@ -15,7 +15,7 @@ public class ToroQuest {
 
 	
 	public static final String MODID = "toroquest";
-	public static final String VERSION = "1.10.2-1";
+	public static final String VERSION = "1.10.2-3";
 	public static final String MODNAME = "ToroQuest";
 	
 	@SidedProxy(clientSide = "net.torocraft.toroquest.ClientProxy", serverSide = "net.torocraft.toroquest.ServerProxy")

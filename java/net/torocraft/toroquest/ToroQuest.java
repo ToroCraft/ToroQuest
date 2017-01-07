07715860b2f5bbdbc7db072cabe1c52c7e457494
Charles Howard <cdhoward@add123.com>
2017-01-07 11:33:41 -0500

    version step to a6

--- java/net/torocraft/toroquest/ToroQuest.java
+++ java/net/torocraft/toroquest/ToroQuest.java
@@ -14,7 +14,7 @@ public class ToroQuest {
 
 	
 	public static final String MODID = "toroquest";
-	public static final String VERSION = "1.11.2-a5";
+	public static final String VERSION = "1.11.2-a6";
 	public static final String MODNAME = "ToroQuest";
 	
 	@SidedProxy(clientSide = "net.torocraft.toroquest.ClientProxy", serverSide = "net.torocraft.toroquest.ServerProxy")

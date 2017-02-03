dde7306c68b58eb8713752bfa2df279353efd481
Charles Howard <frodare@gmail.com>
2017-02-02 21:26:32 -0500

    version step to 2-a3

--- java/net/torocraft/toroquest/ToroQuest.java
+++ java/net/torocraft/toroquest/ToroQuest.java
@@ -17,7 +17,7 @@ public class ToroQuest {
 
 	
 	public static final String MODID = "toroquest";
-	public static final String VERSION = "1.11.2-2-a2";
+	public static final String VERSION = "1.11.2-2-a3";
 	public static final String MODNAME = "ToroQuest";
 	
 	@SidedProxy(clientSide = "net.torocraft.toroquest.ClientProxy", serverSide = "net.torocraft.toroquest.ServerProxy")

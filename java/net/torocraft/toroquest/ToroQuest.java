a21c855ea2b69d0e1783fa464cd763a1afa7d0db
Charles Howard <frodare@gmail.com>
2017-02-27 08:45:36 -0500

    version step

--- java/net/torocraft/toroquest/ToroQuest.java
+++ java/net/torocraft/toroquest/ToroQuest.java
@@ -11,43 +11,41 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
 import net.torocraft.toroquest.configuration.ConfigurationHandler;
 
-@Mod (modid = ToroQuest.MODID, name = ToroQuest.MODNAME, version = ToroQuest.VERSION,
-		guiFactory = "net.torocraft." + ToroQuest.MODID + ".gui.GuiFactoryToroQuest")
+@Mod(modid = ToroQuest.MODID, name = ToroQuest.MODNAME, version = ToroQuest.VERSION, guiFactory = "net.torocraft." + ToroQuest.MODID
+		+ ".gui.GuiFactoryToroQuest")
 public class ToroQuest {
 
-	
 	public static final String MODID = "toroquest";
-	public static final String VERSION = "1.11.2-3.1";
+	public static final String VERSION = "1.11.2-3.2";
 	public static final String MODNAME = "ToroQuest";
-	
+
 	@SidedProxy(clientSide = "net.torocraft.toroquest.ClientProxy", serverSide = "net.torocraft.toroquest.ServerProxy")
 	public static CommonProxy proxy;
-	
+
 	@Instance(value = ToroQuest.MODID)
 	public static ToroQuest INSTANCE;
-	
+
 	@EventHandler
 	public void preInit(FMLPreInitializationEvent e) {
-	    proxy.preInit(e);
-	    
-	    ConfigurationHandler.init(e.getSuggestedConfigurationFile());
+		proxy.preInit(e);
+
+		ConfigurationHandler.init(e.getSuggestedConfigurationFile());
 		MinecraftForge.EVENT_BUS.register(new ConfigurationHandler());
 	}
 
 	@EventHandler
 	public void init(FMLInitializationEvent e) {
-	    proxy.init(e);
+		proxy.init(e);
 	}
 
 	@EventHandler
 	public void postInit(FMLPostInitializationEvent e) {
-	    proxy.postInit(e);
+		proxy.postInit(e);
 	}
-	
+
 	@EventHandler
 	public void serverLoad(FMLServerStartingEvent e) {
 		e.registerServerCommand(new ToroQuestCommand());
 	}
-	
-	
+
 }

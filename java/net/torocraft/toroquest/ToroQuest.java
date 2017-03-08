77ea4cf0a515ab77013fe9b9538934d092f686a1
Charles Howard <frodare@gmail.com>
2017-03-07 20:53:00 -0500

    version step to 3.3

--- java/net/torocraft/toroquest/ToroQuest.java
+++ java/net/torocraft/toroquest/ToroQuest.java
@@ -16,7 +16,7 @@ import net.torocraft.toroquest.configuration.ConfigurationHandler;
 public class ToroQuest {
 
 	public static final String MODID = "toroquest";
-	public static final String VERSION = "1.11.2-3.2";
+	public static final String VERSION = "1.11.2-3.3";
 	public static final String MODNAME = "ToroQuest";
 
 	@SidedProxy(clientSide = "net.torocraft.toroquest.ClientProxy", serverSide = "net.torocraft.toroquest.ServerProxy")

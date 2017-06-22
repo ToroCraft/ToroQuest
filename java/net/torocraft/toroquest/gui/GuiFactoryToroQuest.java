1f178dba44714cdb9b48db136bb6a35995029f0f
Charles Howard <frodare@gmail.com>
2017-06-22 19:51:44 -0400

    forge upgrade

--- java/net/torocraft/toroquest/gui/GuiFactoryToroQuest.java
+++ java/net/torocraft/toroquest/gui/GuiFactoryToroQuest.java
@@ -14,6 +14,16 @@ public class GuiFactoryToroQuest implements IModGuiFactory {
 	}
 
 	@Override
+	public boolean hasConfigGui() {
+		return true;
+	}
+
+	@Override
+	public GuiScreen createConfigGui(GuiScreen parentScreen) {
+		return new GuiConfigToroQuest(parentScreen);
+	}
+
+	@Override
 	public Class<? extends GuiScreen> mainConfigGuiClass() {
 		return GuiConfigToroQuest.class;
 	}
